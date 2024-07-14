87bec1543be82cb812ff7621cec595599db274b0
Alena Kozyreva <alena.kozyreva@paygine.net>
2024-07-14 22:38:18 +0300

    add dz11 test

--- src/test/java/pro/sky/demo2/service/DepartmentServiceImplTestsDepartmets.java
+++ src/test/java/pro/sky/demo2/service/DepartmentServiceImplTestsDepartmets.java
@@ -15,60 +15,61 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 
-
-
 public class DepartmentServiceImplTestsDepartmets {
 
 
-	private EmployeeServiceImpl employeeService;
+    private EmployeeServiceImpl employeeService;
 
-   private DepartmentService departmentService;
-   @BeforeEach
-   public void init(){
-		employeeService= Mockito.mock(EmployeeServiceImpl.class);
-				departmentService=new DepartmentServiceImpl(employeeService);
-	}
-
+    private DepartmentService departmentService;
     private List<Employee> employeeList = List.of(
-			new Employee("Иванов", "Иван", 22222, 3),
-			new Employee("Сидоров", "Федя", 33333, 3),
-			new Employee("Кросс", "Тони", 11111, 1));
+            new Employee("Иванов", "Иван", 22222, 3),
+            new Employee("Сидоров", "Федя", 33333, 3),
+            new Employee("Кросс", "Тони", 11111, 1));
+
+    @BeforeEach
+    public void init() {
+        employeeService = Mockito.mock(EmployeeServiceImpl.class);
+        departmentService = new DepartmentServiceImpl(employeeService);
+    }
 
     @Test
-    public void byDepartment (){
-		when(employeeService.getAll()).thenReturn(employeeList);
+    public void byDepartment() {
+        when(employeeService.getAll()).thenReturn(employeeList);
 
-		Map<Integer, List<Employee>> employees= departmentService.getByDepartment();
-		assertEquals(2, employees.size());
-		assertEquals(2, employees.get(3).size());
-		assertEquals(1, employees.get(1).size());
+        Map<Integer, List<Employee>> employees = departmentService.getByDepartment();
+        assertEquals(2, employees.size());
+        assertEquals(2, employees.get(3).size());
+        assertEquals(1, employees.get(1).size());
 
-	}
-	@Test
-	public void byDepartmentAll (){
-		when(employeeService.getAll()).thenReturn(employeeList);
-		Collection<Employee> employees=departmentService.getByDepartmentAll(2);
-		assertEquals(0, employees.size());
-		when(employeeService.getAll()).thenReturn(employeeList);
-		Collection<Employee> employees2=departmentService.getByDepartmentAll(3);
-		assertEquals(2, employees2.size());
-		assertEquals(employeeList.stream().findFirst(), employees2.stream().findFirst());
+    }
 
-	}
-	@Test
-	public void byDepartmentSallaryMin (){
-		when(employeeService.getAll()).thenReturn(employeeList);
-		Employee employees=departmentService.minSalary(3);
-		assertEquals(22222, employees.getSallary());
-		when(employeeService.getAll()).thenReturn(employeeList);
-		assertThrows(EmployeeNotFoundException.class ,() -> departmentService.minSalary(2));
-	}
-	@Test
-	public void byDepartmentSallaryMax (){
-		when(employeeService.getAll()).thenReturn(employeeList);
-		Employee employees=departmentService.maxSalary(3);
-		assertEquals(33333, employees.getSallary());
-		when(employeeService.getAll()).thenReturn(employeeList);
-		assertThrows(EmployeeNotFoundException.class ,() -> departmentService.maxSalary(2));
-	}
+    @Test
+    public void byDepartmentAll() {
+        when(employeeService.getAll()).thenReturn(employeeList);
+        Collection<Employee> employees = departmentService.getByDepartmentAll(2);
+        assertEquals(0, employees.size());
+        when(employeeService.getAll()).thenReturn(employeeList);
+        Collection<Employee> employees2 = departmentService.getByDepartmentAll(3);
+        assertEquals(2, employees2.size());
+        assertEquals(employeeList.stream().findFirst(), employees2.stream().findFirst());
+
+    }
+
+    @Test
+    public void byDepartmentSallaryMin() {
+        when(employeeService.getAll()).thenReturn(employeeList);
+        Employee employees = departmentService.minSalary(3);
+        assertEquals(22222, employees.getSallary());
+        when(employeeService.getAll()).thenReturn(employeeList);
+        assertThrows(EmployeeNotFoundException.class, () -> departmentService.minSalary(2));
+    }
+
+    @Test
+    public void byDepartmentSallaryMax() {
+        when(employeeService.getAll()).thenReturn(employeeList);
+        Employee employees = departmentService.maxSalary(3);
+        assertEquals(33333, employees.getSallary());
+        when(employeeService.getAll()).thenReturn(employeeList);
+        assertThrows(EmployeeNotFoundException.class, () -> departmentService.maxSalary(2));
+    }
 }
--- src/test/java/pro/sky/demo2/service/EmployeeServiceImplTestsDepartmets.java
+++ src/test/java/pro/sky/demo2/service/EmployeeServiceImplTestsDepartmets.java
@@ -12,55 +12,56 @@ import java.util.Collection;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EmployeeServiceImplTestsDepartmets {
-	private EmployeeService employeeService = new EmployeeServiceImpl();
+    private EmployeeService employeeService = new EmployeeServiceImpl();
 
 
-	@Test
-	public void getAddEmployees() {
+    @Test
+    public void getAddEmployees() {
 
-		Employee employee = employeeService.addEmployee("vsd", "Dddd", 2, 2);
+        Employee employee = employeeService.addEmployee("vsd", "Dddd", 2, 2);
 
-		assertEquals("Vsd Dddd", employee.getFullName());
-		assertEquals(2, employee.getSallary());
-		assertEquals(2, employee.getDepartment());
-		assertNotEquals("Vsd !", employee.getFullName());
-		assertNotEquals("222 Dddd", employee.getFullName());
-		assertNotEquals(1, employee.getSallary());
-		assertThrows(EmployeeStorageIsFullException.class,()->	employeeService.addEmployee("vsd", "dddd", 5, 2));
-	}
+        assertEquals("Vsd Dddd", employee.getFullName());
+        assertEquals(2, employee.getSallary());
+        assertEquals(2, employee.getDepartment());
+        assertNotEquals("Vsd !", employee.getFullName());
+        assertNotEquals("222 Dddd", employee.getFullName());
+        assertNotEquals(1, employee.getSallary());
+        assertThrows(EmployeeStorageIsFullException.class, () -> employeeService.addEmployee("vsd", "dddd", 5, 2));
+    }
 
-	@Test
-	public void removeEmployee() {
+    @Test
+    public void removeEmployee() {
 
-		employeeService.addEmployee("vsd", "dddd", 5, 2);
-		employeeService.addEmployee("qsd", "dddcdd", 2, 2);
-		employeeService.removeEmployee("qsd", "dddcdd", 2, 2);
-		assertThrows(EmployeeNotFoundException.class, () ->
-				employeeService.removeEmployee("qsd", "dddc3dd", 2, 2));
-		assertThrows(EmployeeNotFoundException.class, () ->
-				employeeService.findEmployee("qsd", "dddcdd", 2, 2));
+        employeeService.addEmployee("vsd", "dddd", 5, 2);
+        employeeService.addEmployee("qsd", "dddcdd", 2, 2);
+        employeeService.removeEmployee("qsd", "dddcdd", 2, 2);
+        assertThrows(EmployeeNotFoundException.class, () ->
+                employeeService.removeEmployee("qsd", "dddc3dd", 2, 2));
+        assertThrows(EmployeeNotFoundException.class, () ->
+                employeeService.findEmployee("qsd", "dddcdd", 2, 2));
 
-	}
+    }
 
-	@Test
-	public void findEmployee() {
-		employeeService.addEmployee("vsd", "dddd", 5, 2);
-		Employee employee=employeeService.findEmployee("vsd", "dddd", 5, 2);
+    @Test
+    public void findEmployee() {
+        employeeService.addEmployee("vsd", "dddd", 5, 2);
+        Employee employee = employeeService.findEmployee("vsd", "dddd", 5, 2);
 
-		assertEquals(("Vsd"),
-				 employee.getFirstName());
-		assertEquals(("Dddd"),
-				employee.getSecondName());
-		assertNotEquals("555",
-				employee.getSallary());
-	}
-	@Test
-	public void allEmployee() {
-		Employee employee1 =employeeService.addEmployee("vsd", "dddd", 5, 2);
-		Collection <Employee> employeeCol=employeeService.getAll();
-		assertEquals(employee1,employeeCol.stream().findFirst().get());
-		assertNotEquals(2,
-				employeeCol.size());
-		MatcherAssert.assertThat(employeeCol, Matchers.containsInAnyOrder(employee1));
-	}
+        assertEquals(("Vsd"),
+                employee.getFirstName());
+        assertEquals(("Dddd"),
+                employee.getSecondName());
+        assertNotEquals("555",
+                employee.getSallary());
+    }
+
+    @Test
+    public void allEmployee() {
+        Employee employee1 = employeeService.addEmployee("vsd", "dddd", 5, 2);
+        Collection<Employee> employeeCol = employeeService.getAll();
+        assertEquals(employee1, employeeCol.stream().findFirst().get());
+        assertNotEquals(2,
+                employeeCol.size());
+        MatcherAssert.assertThat(employeeCol, Matchers.containsInAnyOrder(employee1));
+    }
 }
