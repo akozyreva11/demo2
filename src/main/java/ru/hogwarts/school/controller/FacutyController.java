49eb047d2a64530a1c11c50e5e0b839889243174
Alena Kozyreva <alena.kozyreva@paygine.net>
2024-09-10 01:22:18 +0300

    3.4 dz

--- src/main/java/ru/hogwarts/school/controller/FacutyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacutyController.java
@@ -3,8 +3,10 @@ package ru.hogwarts.school.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.FacultyService;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -17,12 +19,12 @@ public class FacutyController {
         this.facultyService = facultyService;
     }
 
-    @PostMapping
+    @PostMapping("creater")
     public Faculty createFacultyes(@RequestBody Faculty faculty) {
         return facultyService.createFaculty(faculty);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("id")
     public ResponseEntity<Faculty> getFacultyes(@PathVariable Long id) {
         Faculty newFaculty = facultyService.findFaculty(id);
         if (newFaculty == null) {
@@ -31,7 +33,7 @@ public class FacutyController {
         return ResponseEntity.ok(newFaculty);
     }
 
-    @PutMapping("{id}")
+    @PutMapping("id")
     public ResponseEntity<Faculty> updateFacultyes(@RequestBody Faculty faculty, @PathVariable Long id) {
         Faculty changeFaculty = facultyService.editFaculty(id, faculty);
         if (changeFaculty == null) {
@@ -40,13 +42,28 @@ public class FacutyController {
         return ResponseEntity.ok(changeFaculty);
     }
 
-    @DeleteMapping("{id}")
+    @GetMapping ("name")
+    public List <Faculty> findColorFaculty (@RequestParam String name)
+    {
+    return facultyService.findFacultyColor(name);
+    }
+
+    @GetMapping("getFacultyAll")
+    public ResponseEntity<Collection<Student>> getFacultyesByStudents(@PathVariable Long id) {
+        Faculty faculty = facultyService.findFaculty(id);
+        if (faculty == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(facultyService.findFaculty(id).getStudents());
+    }
+
+    @DeleteMapping("id")
     public ResponseEntity<Faculty> deleteFacultyes(@PathVariable Long id) {
         facultyService.deleteFaculty(id);
         return ResponseEntity.ok().build();
     }
 
-    @RequestMapping
+    @RequestMapping("coloradd")
     public List<Faculty> collorsOfFaculty(@RequestParam String collor) {
         return facultyService.facultyCollor(collor);
     }
--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -1,60 +1,69 @@
 package ru.hogwarts.school.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
 import java.util.Objects;
 
-@Entity(name = "faculty")
+@Entity(name = "facultys")
 
 public class Faculty {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String name;
     private String color;
+
+    @OneToMany(mappedBy = "faculty")
+    @JsonIgnore
+    private List<Student> students;
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
 
     public Faculty() {
     }
 
-    public Faculty(Long id, String name, String color) {
+    public Faculty(long id, String name, String color) {
         this.id = id;
         this.name = name;
         this.color = color;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
     }
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getColor() {
         return color;
     }
-
     public void setColor(String color) {
         this.color = color;
     }
-
     @Override
-    public final boolean equals(Object o) {
+    public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Faculty faculty)) return false;
-
-        return Objects.equals(getId(), faculty.getId()) && Objects.equals(getName(), faculty.getName()) && Objects.equals(getColor(), faculty.getColor());
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return Objects.equals(id, faculty.id) && Objects.equals(name, faculty.name) && Objects.equals(color, faculty.color);
     }
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
+    }
     @Override
     public String toString() {
         return "Faculty{" +
@@ -62,13 +71,5 @@ public class Faculty {
                 ", name='" + name + '\'' +
                 ", color='" + color + '\'' +
                 '}';
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hashCode(getId());
-        result = 31 * result + Objects.hashCode(getName());
-        result = 31 * result + Objects.hashCode(getColor());
-        return result;
     }
 }
